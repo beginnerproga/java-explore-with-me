b17282c9fc90993d42b777b01e41f01762dbf041
Nikita <kim.nikituschcka@yandex.ru>
2023-01-10 15:20:34 +0300

    develop

--- stats-server/src/main/java/ru/practicum/repositories/EndpointHitRepository.java
+++ stats-server/src/main/java/ru/practicum/repositories/EndpointHitRepository.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.models.ViewStats(e.app, e.uri, COUNT (e.ip)) " +
-            "from EndpointHit e WHERE e.timestamp> ?1 AND e.timestamp< ?2 and e.uri in ?3 GROUP BY e.app, e.uri")
+            "from EndpointHit e WHERE e.timestamp>= ?1 AND e.timestamp<= ?2 and e.uri in ?3 GROUP BY e.app, e.uri")
     List<ViewStats> findAll(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.models.ViewStats(e.app, e.uri, COUNT (DISTINCT e.ip)) from " +
-            "EndpointHit e WHERE e.timestamp> ?1 AND e.timestamp< ?2 and e.uri in ?3 GROUP BY e.app, e.uri")
+            "EndpointHit e WHERE e.timestamp>= ?1 AND e.timestamp<= ?2 and e.uri in ?3 GROUP BY e.app, e.uri")
     List<ViewStats> findAllUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
 
